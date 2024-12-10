ea0a8b574075b0519a12648eb2fbb36a9fcbf8ed
Arafat <myarafat.ctg@gmail.com>
2024-12-10 21:53:57 +0600

    e-1

--- src/pages/Add.js
+++ src/pages/Add.js
@@ -1,10 +1,65 @@
 import React from 'react'
+import { assets } from '../assets/assets'
 
 const Add = () => {
+  
+
   return (
-    <div>
-      <p>Add Student is under development</p>
-    </div>
+    <form>
+      <div className='flex flex-col w-full items-start gap-3 mb-2'>
+        <p className='mb-2'>Upload Image</p>
+        <label htmlFor="image">
+          <img className='w-[100px]' src={assets.upload_area} alt="" />
+          <input type="file" id='image' hidden/>
+        </label>
+      </div>
+      <div className='w-full'>
+        <p className='mb-2'>Student name</p>
+        <input className='w-full max-w-[500px] px-3 py-2' type="text" name="" id="" placeholder='Type here' required/>
+      </div>
+
+      <div className='w-full'>
+        <p className='mb-2'>Parent name</p>
+        <input className='w-full max-w-[500px] px-3 py-2' type="text" name="" id="" placeholder='Type here' required/>
+      </div>
+
+      <div className='w-full'>
+        <p className='mb-2'>Class</p>
+        <input className='w-full max-w-[500px] px-3 py-2' type="text" name="" id="" placeholder='Type here' required/>
+      </div>
+
+      <div className='w-full'>
+        <p className='mb-2'>Section</p>
+        <input className='w-full max-w-[500px] px-3 py-2' type="text" name="" id="" placeholder='Type here' required/>
+      </div>
+
+      <div className='w-full'>
+        <p className='mb-2'>Id No</p>
+        <input className='w-full max-w-[500px] px-3 py-2' type="text" name="" id="" placeholder='Type here' required/>
+      </div>
+
+      <div className='w-full'>
+        <p className='mb-2'>Class Roll</p>
+        <input className='w-full max-w-[500px] px-3 py-2' type="text" name="" id="" placeholder='Type here' required/>
+      </div>
+
+      <div className='w-full'>
+        <p className='mb-2'>Birthday</p>
+        <input className='w-full max-w-[500px] px-3 py-2' type="text" name="" id="" placeholder='Type here' required/>
+      </div>
+
+      <div className='w-full'>
+        <p className='mb-2'>Valid Till</p>
+        <input className='w-full max-w-[500px] px-3 py-2' type="text" name="" id="" placeholder='Type here' required/>
+      </div>
+
+      <div className='w-full'>
+        <p className='mb-2'>Gender</p>
+        <input className='w-full max-w-[500px] px-3 py-2' type="text" name="" id="" placeholder='Type here' required/>
+      </div>
+
+      <button type='submit' className='w-28 py-3 mt-4 bg-black text-white'>Enroll Student</button>
+    </form>
   )
 }
 
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -4,14 +4,24 @@ import { assets } from '../assets/assets';
 
 const Login = ({ setToken }) => {
   const [email, setEmail] = useState('');
-  const [passwrd, setPassword] = useState('');
+  const [password, setPassword] = useState('');
 
   const onSubmitHandler = async (e) => {
     e.preventDefault();
     console.log('email', email);
-    console.log('passwrd', passwrd);
-    setToken(email);
-    toast.success("Login successfully");
+    console.log('password', password);
+    try {
+        const response = await fetch(`http://182.163.101.182:9080/fdml/edu/eeom/auth/getAuth?userName=${email}&password=${password}`); 
+        const data = await response.json();
+        if(data.success){
+          setToken(data.token);
+          toast.success("Login successfully");
+        }else{
+          toast.error(data.message);
+        }
+    } catch (error) {
+      console.error('Error submitting form:', error);
+    }
   };
 
   return (
@@ -27,7 +37,7 @@ const Login = ({ setToken }) => {
               onChange={(e) => setEmail(e.target.value)}
               value={email}
               className='rounded-md w-full px-3 py-2 border border-gray-300 outline-none'
-              type="email"
+              type="text"
               placeholder='your@email.com'
               required />
           </div>
@@ -35,7 +45,7 @@ const Login = ({ setToken }) => {
             <p className='text-sm font-medium text-gray-700 mb-2'>Password</p>
             <input
               onChange={(e) => setPassword(e.target.value)}
-              value={passwrd}
+              value={password}
               className='rounded-md w-full px-3 py-2 border border-gray-300 outline-none'
               type="password"
               placeholder='Enter your password'
